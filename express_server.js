441e693277f4215edd2ebb7183bf6ac87d5c37a4
Charles Denahy-Knowles <cjknowles90@gmail.com>
2023-10-02 22:01:28 -0700

    Updated expressServer.js to store short-url and long-url key value pairs to database when a POST request is received to /urls.Also updated it to respond with a redirect from /urls to /urls/:id. Pair programmed with The Great... @stephen-fraser.

--- express_server.js
+++ express_server.js
@@ -1,12 +1,23 @@
 const express = require("express");
 const app = express();
-const PORT = 8080;
+const PORT = 8080; // default port 8080
 
-app.use(express.urlencoded({ extended: true}));
 
+// configuration of express app
 app.set("view engine", "ejs");
 
-function generateRandomString() {}; 
+// POST requests are sent as a BUFFER (great for transmitting data but isnt readable without this)
+app.use(express.urlencoded({ extended: true})); // this is middleware
+// random string generator to simulate tinyURL
+
+function generateRandomString() {    
+  let result = '';
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+for (let x = 0; x < 6; x++) {
+  result += characters.charAt(Math.floor(Math.random() * characters.length));
+}
+return result;
+}; 
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -17,39 +28,66 @@ app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
 
+app.post("/urls", (req, res) => {
+  const shortURLId = generateRandomString();
+  const longURL = req.params.id;
+  urlDatabase[req.body.longURL] = longURL;
+  res.redirect(`/urls/${shortURLId}`);
+});
+
+// logs the POST request body to the console and responds 
+// app.post("/urls/:id", (req, res) => {
+//   const shortURLId = generateRandomString();
+//   const longURL = req.body.longURL;
+//   urlDatabase[req.body.longURL] = longURL;
+//   res.redirect(`/urls/${shortURLId}`);
+// });
+
+// redirect any request to ("u/:id") to its longURL
+app.get("/u/:id", (req, res) => {
+  const longURL = urlDatabase[req.params.id];
+  res.redirect(longURL);
+});
+
+// new route handler for /urls
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
 
-app.get("urls", (req, res) => {
-  console.log(req.body);
-  res.send("Ok");
-});
-
+// new route for /urls/new - the form
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
-
+// new route for URL tinyIDs
 app.get("/urls/:id", (req, res) => {
-  const templateVars = { id: req.params.id,         longURL: urlDatabase[req.params.id]};
+  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id]};
   res.render("urls_show", templateVars);
 });
-
-// app.get("?urls/:id:", (req, res) => {
-//   const templateVars = { id: "9sm5xk" , longURL: "http://www.google.com" };
-//   res.render("urls_show", templateVars);
-// });
 
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
+// adds JSON string that reprents the entire urlDatabase objects at time of request
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
+// displays HTML content that the /hello path responds with
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
+
+// set path created to display scope
+app.get("/set", (req, res) => {
+  const a = 1;
+  res.send(`a = ${a}`);
+ });
+
+// fetch path created to show erro that you can't access value of a because of reference/scope error
+app.get("/fetch", (req, res) => {
+res.send(`a = ${a}`);
+});
+
 
