3681a97abf3e3c482a52da9d7f018c2adf0ae8ef
Charles Denahy-Knowles <cjknowles90@gmail.com>
2023-10-05 10:03:15 -0700

    Created a function to getUserUrls - only display if the user is logged in and only show urls that belong to that user. Else - prompt the user to register/login first. Updated the /edit & /delete endpoints so only the owners/creators of those urls can edit/delete links.

--- express_server.js
+++ express_server.js
@@ -11,12 +11,11 @@ const PORT = 8080; // default port 8080
 app.set("view engine", "ejs");
 
 // this is middleware
+// allowing cookies to be stored and used by the server
+// morgan is middleware to test URL/GET/POST connections/errors
+app.use(cookieParser()); // creates req.cookies
 app.use(express.urlencoded({ extended: true})); // creates req.body vs (false)???
 app.use(morgan('dev')); // (req, res, next) vs ('tiny')???
-// morgan is middleware to test URL/GET/POST connections/errors
-
-// allowing cookies to be stored and used by the server
-app.use(cookieParser()); // creates req.cookies
 
 // random string generator to simulate tinyURL
 const generateRandomString = (length) => Math.random().toString(36).substring(2, (length + 2)); // generates a random 6 character string
@@ -32,46 +31,179 @@ const getUserByEmail = (users, email) => {
   return null;
 };
 
-// databases for URLs and usernames
-const urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xk": "http://www.google.com"
+// function similar to above - but to filter through urls associated with userID's
+const getUserUrls = (urlDatabase, userID) => {
+  const newUrls = {};
+
+  for (let url in urlDatabase) {
+    if (urlDatabase[url].userID === userID) {
+      newUrls[url] = urlDatabase[url];
+    }
+  };
+  return newUrls;
 };
 
-const users = { happyTimes: {
-  id: "happyTimes",
+// databases for URLs and usernames
+const urlDatabase = {
+  b2xVn2: {
+    longURL: "http://www.lighthouselabs.ca",
+    userID: "b2xVn2",
+},
+  e9m5xk: {
+    longURL: "http://www.google.com",
+    userID: "e9m5xk",
+},
+};
+
+// new updated database for the users info: id, email, password
+const users = { b2xVn2: {
+  id: "b2xVn2",
   email: "user@example.com",
-  password: "purple-monkey-dinosaur",
+  password: "123454321",
 },
-cknowles90: {
-  id: "cknowles90",
+e9m5xk: {
+  id: "e9m5xk",
   email: "user2@example.com",
-  password: "dishwasher-funk",
+  password: "password",
 },
-};;
+};
 
 
 // POST requests are sent as a BUFFER (great for transmitting data but isnt readable without this)
-// logs the POST request body to the console and responds 
-app.post('/urls/:id', (req, res) => {
-  const templateVars = {id: req.params.id, longURL: urlDatabase[req.params.id]}
-  res.render('urls_show', templateVars)
+// GET requests information/data from the browser and servers, and returns to the client
+
+
+// redirects to a page with the shortURLId
+app.post("/urls", (req, res) => {
+  console.log("POST /urls route handler called");
+  const userID = req.cookies["user.id"];
+
+  if (!userID) { // cheeky hyperlink within message to redirect to /register page. Thank-you stackOverflow and html in-line CSS;
+    return res.status(400).send(`<center><a href="/register">Join Us!</a><br>If you want Tiny URLs...</center>`)
+  }
+
+  const shortURLId = generateRandomString(6);
+  const longURL = req.body.longURL;
+  console.log("shortURLId:", shortURLId);
+  console.log("longURL:", longURL);
+
+  urlDatabase[shortURLId] = longURL;
+
+  console.log("urlDatabase:", urlDatabase);
+  res.redirect(`/urls/${shortURLId}`);
 });
 
-// deletes the stored URL id, longURL and shortID URL from the urlDatabase
-app.post('/urls/:id/delete', (req, res) => {
-  const idToDelete = req.params.id;
-  delete urlDatabase[idToDelete]; 
-  res.redirect('/urls');
+// new route handler for /urls - Added USERNAME and cookies
+app.get("/urls", (req, res) => {
+  
+  const userID = req.cookies["user.id"];
+  const userUrls = getUserUrls(urlDatabase, userID);
+  console.log("userURLs:", userUrls);
+  
+  const templateVars = { 
+    user: users[userID],
+    urls: userUrls,
+    urlDatabase: urlDatabase,
+  };
+
+  res.render("urls_index", templateVars);
+});
+
+// new route for /urls/new - the form
+app.get("/urls/new", (req, res) => {
+  if (!req.cookies["user.id"]) {
+    res.redirect("/login");
+  } else {
+  const templateVars = {
+    user: users[req.cookies["user.id"]]
+  };
+  res.render("urls_new", templateVars);
+  }
 });
 
 // replaces the old longURL with the new input URL - new Edit feature/page
 app.post('/urls/:id', (req, res) => {
-  const idToUpdate = req.params.id;
-  const newLongURL = req.body.longURL;
+
+  const userID = req.cookies["user.id"];
+  const user = users[userID];
+
+  const id = req.params.id;
+  const newLongURL = req.body.newLongURL;
+  const destinationURL = urlDatabase[id];
+
+  if (!destinationURL) {
+    return res.status(400).send("Invalid short URL");
+  }
+
+  if (!user) {
+    return res.status(400).send("You must be logged in as a registered Tiny APP member to access this content");
+  }
+
+  if (user.id !== destinationURL.userID) {
+    return res.status(401).send("Access Denied: This URL does not belong to you. Nice try!")
+  }
   
-  urlDatabase[idToUpdate] = newLongURL;
+  urlDatabase[id].longURL = newLongURL;
   
+  res.redirect('/urls');
+});
+
+// new route for URL tinyIDs
+app.get("/urls/:id", (req, res) => {
+
+  const userID = req.cookies["user.id"];
+  const user = users[userID];
+
+  if (!user) {
+    return res.status(400).send("You must be logged in to access this content");
+  }
+
+  if (user.id !== urlDatabase[req.params.id].userID) {
+    return res.status(401).send("Access Denied: This URL does not belong to you!")
+  }
+
+  const templateVars = { 
+    id: req.params.id, 
+    longURL: urlDatabase[req.params.id].longURL,
+    user: users[req.cookies["user.id"]]
+  };
+  res.render("urls_show", templateVars);
+});
+
+// redirect any request to ("u/:id") to its longURL
+app.get("/u/:id", (req, res) => {
+  const longURL = urlDatabase[req.params.id].longURL;
+
+  if (!longURL) {
+    const errorMessage = "Invalid Tiny URL.";
+    return res.status(404).render("error", { errorMessage });
+  }
+  res.redirect(longURL);
+});
+
+// deletes the stored URL id, longURL and shortID URL from the urlDatabase
+app.post('/urls/:id/delete', (req, res) => {
+
+  const user = users[req.cookies['user.id']];
+  const id = req.params.id;
+  const destinationURL = urlDatabase[id];
+
+  if (!destinationURL) {
+    return res.status(400).send("Invalid short URL");
+  }
+
+  if (!user) {
+    return res.status(400).send("You must be logged in as a registered Tiny APP member to access this content");
+  }
+
+  if (user.id !== destinationURL.userID) {
+    return res.status(401).send("Access Denied: This URL does not belong to you. Nice try!")
+  }
+
+  const idToDelete = req.params.id;
+
+  delete urlDatabase[idToDelete]; 
+
   res.redirect('/urls');
 });
 
@@ -87,22 +219,21 @@ app.post('/login', (req, res) => {
   res.redirect("/urls");
 });
 
+app.get('/login', (req, res) => {
+  if (req.cookies["user.id"]) {
+    res.redirect("/urls");
+  } else {
+  const templateVars = {
+    user: users[req.cookies["user.id"]]
+  };
+  res.render("urls_login", templateVars);
+  }
+});
+
 // gives a logout endpoint and clears the username cookie - redirects back to /urls
 app.post('/logout', (req, res) => {
   res.clearCookie("user.id");
   res.redirect("/login");
-});
-
-// redirects to a page with the shortURLId
-app.post("/urls", (req, res) => {
-  if (!req.cookies["user.id"]) { // cheeky hyperlink within message to redirect to /register page. Thank-you stackOverflow and html in-line CSS;
-    return res.status(400).send(`<center><a href="/register">Join Us!><br>If you want Tiny URLs...</a></center>`)
-  } else {
-  const shortURLId = generateRandomString(6);
-  const longURL = req.body.longURL;
-  urlDatabase[shortURLId] = longURL;
-  res.redirect(`/urls/${shortURLId}`);
-}
 });
 
 // registration request for email and password
@@ -127,101 +258,35 @@ app.post('/register', (req, res) => {
     email: email,
     password: password
   };
-  
+
   users[id] = user;
-  console.log(users);
- 
+
   res.cookie("user.id", user.id);
-  res.redirect("/urls");
-});
-
-
-// GET requests information/data from the browser and servers, and returns to the client
-// new route handler for /urls - Added USERNAME and cookies
-app.get("/urls", (req, res) => {
-  const templateVars = { 
-    urls: urlDatabase,
-    user: users[req.cookies["user.id"]]
-  };
-  res.render("urls_index", templateVars);
-});
-
-// new route for /urls/new - the form
-app.get("/urls/new", (req, res) => {
-  if (!req.cookies["user.id"]) {
-    res.redirect("/login");
-  } else {
-  const templateVars = {
-    user: users[req.cookies["user.id"]]
-  };
-  res.render("urls_new", templateVars);
-  }
-});
-
-// redirect any request to ("u/:id") to its longURL
-app.get("/u/:id", (req, res) => {
-  const longURL = urlDatabase[req.params.id];
-
-  if (!longURL) {
-    const errorMessage = "Invalid Tiny URL.";
-    return res.status(404).render("error", { errorMessage });
-  }
-  res.redirect(longURL);
-});
-
-// new route for URL tinyIDs
-app.get("/urls/:id", (req, res) => {
-  const templateVars = { 
-    id: req.params.id, 
-    longURL: urlDatabase[req.params.id],
-    user: users[req.cookies["user.id"]]
-  };
-  res.render("urls_show", templateVars);
+  res.on("end", () => {
+    res.redirect("/urls");
+  });
 });
 
 // new registration page
 app.get("/register", (req, res) => {
   if (req.cookies["user.id"]) {
     res.redirect("/urls");
-  } else {
+  } 
+
   const templateVars = {
     user: users[req.cookies["user.id"]]
   };
   res.render("urls_register", templateVars);
-  }
 });
 
-app.get('/login', (req, res) => {
-  if (req.cookies["user.id"]) {
-    res.redirect("/urls");
-  } else {
-  const templateVars = {
-    user: users[req.cookies["user.id"]]
-  };
-  res.render("urls_login", templateVars);
+app.get('/', (req, res) => {
+
+  if (!users[req.cookies['user.id']]) {
+    res.redirect('/login');
   }
+
+  res.redirect('/urls')
 });
-
-/*
-return res.status(403).send(`User ID and/or email input is invalid. Please check your spelling or <a href="/register>Register Now!</a>`);
-*/
-
-// // get /protected
-// app.get('/protected', (req, res) => {
-
-//   const userId = req.cookies.userId
-
-//   if (!userId) {
-//     return res.status(401).send("Please log in to view this page")
-//   }
-  
-//   const user = username[userId];
-//   const templateVars = {
-//     email: user.email
-//   };
-
-//   res.render('urls_protected', templateVars);
-// });
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
